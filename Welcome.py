915ffc1732c682c0ea3d6eac6478912e57ddd930
inkfinity1 <derek.wu@kingsway.school.nz>
2024-09-18 10:25:17 +1200

    Add files via upload
    Frist coded

---
+++ Welcome.py
@@ -0,0 +1,11 @@
+# My start Python program
+# Author: Derek Wu
+# Date: 2024-09--18
+# Version 1.0
+
+
+#This is a Comment. It starts with a #.
+# Print out a welcome message
+print("Hello World!\n") # \n creates a line break
+print() # this creates a line break
+print('Congratulation on writing your first program')
