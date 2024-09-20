86095f77921bc02a5351420c1269329bb2b806dd
inkfinity1 <derek.wu@kingsway.school.nz>
2024-09-20 14:44:39 +1200

    Add files via upload
    Created  difference type of variable

---
+++ Variables.py
@@ -0,0 +1,27 @@
+# Showing how variables work
+# Aithor: Derek Wu
+# Date: 20 September 2024
+# Version: 1
+
+
+# Different types of variables
+# Store a number
+num_1 = 80 # assigning 80 to num_1
+print(num_1) # This displays what num_1 contains
+
+# Store a string
+name = "Juergen"
+print(name)
+name = "jwe bjkf.hkfebk" # reassinging value to an existing varible]
+print (name) 
+ 
+ # Do calculations in a variable and store the result
+sum=5+5 # Do good practice
+sum = 5 + 5 # follows PEP8 Pytjon conventions
+print(sum)
+sum = "5 + 5 = 10" # this is a string
+print(sum)
+
+# Adding strings together is called  concatenation
+concatenation = name + sum
+print(concatenation)
