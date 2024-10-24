c8b3b8492eca89db12d70bd1961096ed623c7b1d
inkfinity1 <derek.wu@kingsway.school.nz>
2024-10-25 12:54:10 +1300

    Add files via upload
    Error checking

---
+++ Error Checking.py
@@ -0,0 +1,67 @@
+# Error Ckecking
+# Author: Derek Wu
+# Date: 2024/10/16
+# Version 1
+
+# Code that tests that a valid number is entered (V1)
+'''done = False # Boolean variable set to False
+while not done: # while loop that runs until a valid number is entered.
+    num = int(input("Please enter your value:"))
+    done = True
+
+
+
+print(f"The number you entered is {num}.")'''
+
+# Code that tests that a valid number is entered (V2)
+# Create a funtion to call every time I ask the user for
+# a number. A function is a chunk of code that does something,
+# I 'cal' it by writing out its name.
+'''def test_int_num(question):
+    done = False
+    while not done:
+        print(question) # The question a placeholder
+        try: # This tries for a vaild input
+            num = int(input())
+            done = True
+
+        except ValueError:
+            print("That not a valid number.")
+
+
+    return(num) # Gives back the value of num so that I can use it outside the function
+
+# Main routine
+num_1 = test_int_num("Please enter your frist number:")
+print(f"You entered {num_1}")
+
+num_2 = test_int_num("Please enter your second number:")
+print(f" You entered {num_2}")
+
+num_3 = test_int_num("Please enter your third number:")
+print(f" You entered {num_3}")
+
+sum = num_1 + num_2 + num_3
+print(f"The total of {num_1}, {num_2} and {num_3} is {sum}")'''
+
+# Version 3. Refining my code. MAking it more pythonic.
+# Ask the user to pick a number in a range.
+def valid_num(question, low, high):
+    error = f"That is not a vaild number. Please enter an intrger between {low} and {high}"
+    while True:
+        try:
+            response = int(input(question))
+            if low <= response <= high:
+                break
+
+            else:
+                print(error)
+                print()
+
+        except ValueError:
+            print(error)
+    return response
+
+# Main routine
+num_1 = valid_num("Enter a number between 1 and 15:", 1, 15)
+print(f"You entered {num_1}") 
